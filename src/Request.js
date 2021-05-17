0bb8d5a224f8afd5d8de79ec99cbeb89c73993d8
supriya.raghunath96 <supriya.raghunath96@gmail.com>
2021-05-17 20:15:13 +0530

    update

--- src/Request.js
+++ src/Request.js
@@ -187,12 +187,12 @@ class Request extends Component {
       notifBody = "Vaccine Slots are avilable. Hurry up!";
 
     this.playBeep();
-    if (!Notification) {
+    if (("Notification" in window) && !Notification) {
       alert("Desktop notifications not available in your browser");
       return;
     }
 
-    if (Notification.permission !== "granted") Notification.requestPermission();
+    if (("Notification" in window) && Notification.permission !== "granted") Notification.requestPermission();
     else {
       try {
         const notification = new Notification(notifTitle, {
