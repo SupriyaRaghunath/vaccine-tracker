38660f6f92b85136953cd10c74d04e37883b4d14
supriya.raghunath96 <supriya.raghunath96@gmail.com>
2021-05-18 20:44:45 +0530

    optmised App.js

---
+++ src/strings.js
@@ -0,0 +1,9 @@
+const STRINGS = {
+  appTitle: "Vaccine Slot Tracker",
+  findSlot:
+    "Find available slots for your vaccination. If there are no slots,don't worry! we've got your back. We'll notify when there are available slots",
+  startNotifier:
+    "'Start Notifier' and make sure you don't close this tab.",
+};
+
+export { STRINGS };
